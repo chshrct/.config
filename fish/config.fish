7e48213e0042028ded9badd80301b0f3fb896a7d
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-01-02 11:39:51 +0400

    feat(fish): rose-pine colorscheme

--- fish/config.fish
+++ fish/config.fish
@@ -17,7 +17,17 @@ starship init fish | source
 set -gx EDITOR nvim
 
 # fzf
-set -Ux FZF_DEFAULT_OPTS "\
---color=spinner:#f5e0dc,hl:#f38ba8 \
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
+# catpuccin
+# set -Ux FZF_DEFAULT_OPTS "\
+# --color=spinner:#f5e0dc,hl:#f38ba8 \
+# --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+# --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
+# rose-pine
+set -Ux FZF_DEFAULT_OPTS "
+--color=fg:#908caa,hl:#ea9a97
+--color=fg+:#e0def4,hl+:#ea9a97
+--color=border:#44415a,header:#3e8fb0,gutter:#232136
+--color=spinner:#f6c177,info:#9ccfd8
+--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
