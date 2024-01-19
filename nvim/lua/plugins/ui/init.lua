3df321a6e136a1921b13e858ec4bfae4b16aadbf
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-01-19 19:42:49 +0400

    refactor(nvim): remove lazygit

--- nvim/lua/plugins/ui/init.lua
+++ nvim/lua/plugins/ui/init.lua
@@ -96,44 +96,6 @@ return {
     version = "*",
     config = function()
 
-      local Terminal = require("toggleterm.terminal").Terminal
-      local lazygit = Terminal:new({
-        cmd = "lazygit",
-        dir = "git_dir",
-        direction = "float",
-        float_opts = {
-          border = "double",
-        },
-
-        -- function to run on opening the terminal
-        on_open = function(term)
-          vim.cmd("startinsert!")
-          vim.api.nvim_buf_set_keymap(
-            term.bufnr,
-            "n",
-            "q",
-            "<cmd>close<CR>",
-            { noremap = true, silent = true }
-          )
-        end,
-        -- function to run on closing the terminal
-        on_close = function()
-          vim.cmd("startinsert!")
-        end,
-      })
-
-      function _LAZYGIT_TOGGLE()
-        lazygit:toggle()
-      end
-
-      -- Lazygit mapping
-      vim.keymap.set(
-        "n",
-        "<leader>gl",
-        "<cmd>lua _LAZYGIT_TOGGLE()<CR>",
-        { noremap = true, silent = true, desc = "lazy[g]it" }
-      )
-
       -- Terminal window mode mapping
       function _G.set_terminal_keymaps()
         local opts =
