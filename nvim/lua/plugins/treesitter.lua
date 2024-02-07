4048ccf0b8368bccdb21829b35eb919cfa87278e
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-02-07 18:15:19 +0400

    refactor(nvim): remove unused code

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
-      after = "nvim-treesitter/nvim-treesitter",
     },
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
