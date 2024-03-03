05a7187da01510fed018d5def0b99d31e9cf40bd
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-03-03 15:04:51 +0400

    refactor(nvim): use rose-pine moon

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -69,7 +69,7 @@ return {
     priority = 1000,
     config = function()
       require("rose-pine").setup({
-        variant = "auto", -- auto, main, moon, or dawn
+        variant = "moon", -- auto, main, moon, or dawn
         dark_variant = "moon", -- main, moon, or dawn
         dim_inactive_windows = false,
         extend_background_behind_borders = true,
