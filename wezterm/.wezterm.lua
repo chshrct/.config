457b98cbdc3c413b800f9e4b857595d97c848732
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-06-21 16:56:40 +0300

    refactor(wezterm): config update

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,18 +1,20 @@
 local wezterm = require("wezterm")
 local config = {}
 
+-- shell
+config.default_prog = { "pwsh" }
 -- color scheme
 config.color_scheme = "rose-pine-moon"
 
 -- font
-config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 10
+config.font = wezterm.font("MesloLGS Nerd Font")
+config.font_size = 9
 
 -- cursor
 config.force_reverse_video_cursor = true
 
 -- background
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 
 --tab bar
 config.hide_tab_bar_if_only_one_tab = true
