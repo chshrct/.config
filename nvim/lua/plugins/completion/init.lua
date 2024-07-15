c0f57fede05eda7a15efcaa6a86215e01d38288a
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-07-15 23:35:30 +0300

    feat(nvim): copilot x and copilot chat basic setup

--- nvim/lua/plugins/completion/init.lua
+++ nvim/lua/plugins/completion/init.lua
@@ -1,75 +1,105 @@
 return {
-  -- Autocompletion
-  "hrsh7th/nvim-cmp",
-  version = false, -- last release is way too old
-  event = { "InsertEnter", "CmdlineEnter" },
-  dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
-    {
-      "L3MON4D3/LuaSnip",
-      dependencies = {
-        {
-          "rafamadriz/friendly-snippets",
-          config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-          end,
+  {
+    -- Autocompletion
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = { "InsertEnter", "CmdlineEnter" },
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+          {
+            "rafamadriz/friendly-snippets",
+            config = function()
+              require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+          },
         },
       },
+      "saadparwaiz1/cmp_luasnip",
+
+      -- sources
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
     },
-    "saadparwaiz1/cmp_luasnip",
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local icons = require("editor.icons")
 
-    -- sources
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
+      cmp.setup({
+        completion = { completeopt = "menu,menuone,noinsert" },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = require("plugins.completion.keymaps").cmp(cmp, luasnip),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+        formatting = {
+          expandable_indicator = true,
+          fields = { "abbr", "kind" },
+          format = function(_, vim_item)
+            vim_item.kind = (icons.kind[vim_item.kind] or "")
+              .. " "
+              .. vim_item.kind
+            return vim_item
+          end,
+        },
+      })
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ "/", "?" }, {
+        completion = { completeopt = "menu,menuone,noinsert,noselect" },
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(":", {
+        completion = { completeopt = "menu,menuone,noinsert,noselect" },
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end,
   },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    local icons = require("editor.icons")
 
-    cmp.setup({
-      completion = { completeopt = "menu,menuone,noinsert" },
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = require("plugins.completion.keymaps").cmp(cmp, luasnip),
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path" },
-      },
-      formatting = {
-        fields = { "abbr", "kind" },
-        format = function(_, vim_item)
-          vim_item.kind = (icons.kind[vim_item.kind] or "")
-            .. " "
-            .. vim_item.kind
-          return vim_item
-        end,
-      },
-    })
+  -- copilot
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = { "InsertEnter" },
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          auto_trigger = true,
+        },
+      })
+    end,
+  },
 
-    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline({ "/", "?" }, {
-      completion = { completeopt = "menu,menuone,noinsert,noselect" },
-      sources = {
-        { name = "buffer" },
+  -- copilot chat
+  {
+    {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      branch = "canary",
+      dependencies = {
+        { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+        { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
       },
-    })
-
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline(":", {
-      completion = { completeopt = "menu,menuone,noinsert,noselect" },
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        { name = "cmdline" },
-      }),
-    })
-  end,
+      opts = {},
+    },
+  },
 }
