03bf9011cd0a040da5fa1ae899d4d80a7a5e1c20
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-10-16 14:27:45 +0300

    refactor(nvim): extract servers settings

---
+++ nvim/lua/plugins/lsp/servers.lua
@@ -0,0 +1,64 @@
+local M = {}
+
+M.servers = {
+  lua_ls = {
+    settings = {
+      Lua = {
+        -- Tell the language server which version of Lua you're using
+        runtime = {
+          version = "LuaJIT",
+        },
+        diagnostics = {
+          -- Get the language server to recognize the 'vim' global
+          globals = { "vim" },
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        telementary = {
+          enable = false,
+        },
+        hint = {
+          arrayIndex = "Auto",
+          enable = true,
+          setType = true,
+        },
+      },
+    },
+  },
+  -- web
+  cssls = {},
+  html = {},
+  graphql = {},
+  cssmodules_ls = {},
+  tailwindcss = {},
+  eslint = {},
+  emmet_language_server = {},
+  -- Probably want to disable formatting for this lang server
+  ts_ls = {},
+  -- configs
+  jsonls = {
+    settings = {
+      json = {
+        schemas = require("schemastore").json.schemas(),
+        validate = { enable = true },
+      },
+    },
+  },
+  yamlls = {
+    settings = {
+      yaml = {
+        schemaStore = {
+          enable = false,
+          url = "",
+        },
+        schemas = require("schemastore").yaml.schemas(),
+      },
+    },
+  },
+  taplo = {},
+  csharp_ls = {},
+}
+
+return M
