069c69fe69464bf1e07d894474245f26fe1248f6
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-10-22 11:32:04 +0300

    refactor(wezterm): config refactor

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,26 +1,91 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local config = {}
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- term
+config.front_end = "OpenGL"
+config.max_fps = 144
+config.animation_fps = 1
+config.term = "xterm-256color"
+
 -- shell
-config.default_prog = { "pwsh" }
+config.default_prog = { "pwsh", "-NoLogo" }
 -- color scheme
 config.color_scheme = "rose-pine-moon"
 
 -- font
 config.font = wezterm.font("MesloLGS Nerd Font")
 config.font_size = 9
+config.cell_width = 0.9
 
 -- cursor
 config.force_reverse_video_cursor = true
-config.window_decorations = "RESIZE"
+config.default_cursor_style = "BlinkingBlock"
+config.cursor_blink_rate = 500
 
--- background
+-- window
+config.window_decorations = "NONE | RESIZE"
 config.window_background_opacity = 0.9
 
 --tab bar
 config.hide_tab_bar_if_only_one_tab = true
+
+-- keymaps
+config.keys = {
+	{
+		key = "h",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "v",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "U",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "I",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		key = "O",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = "P",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Right", 5 }),
+	},
+	{ key = "9", mods = "CTRL", action = act.PaneSelect },
+	{
+		key = "O",
+		mods = "CTRL|ALT",
+		-- toggling opacity
+		action = wezterm.action_callback(function(window, _)
+			local overrides = window:get_config_overrides() or {}
+			if overrides.window_background_opacity == 1.0 then
+				overrides.window_background_opacity = 0.9
+			else
+				overrides.window_background_opacity = 1.0
+			end
+			window:set_config_overrides(overrides)
+		end),
+	},
+}
 
 return config
