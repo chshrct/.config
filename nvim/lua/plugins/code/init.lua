857619b55747ccb06b56091e8a51bbc98a9105e2
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-11-23 16:14:28 +0300

    refactor(nvim): .

--- nvim/lua/plugins/code/init.lua
+++ nvim/lua/plugins/code/init.lua
@@ -24,31 +24,6 @@ return {
   {
     "HiPhish/rainbow-delimiters.nvim",
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-    config = function()
-      -- This module contains a number of default definitions
-      local rainbow_delimiters = require("rainbow-delimiters")
-
-      vim.g.rainbow_delimiters = {
-        strategy = {
-          [""] = rainbow_delimiters.strategy["global"],
-          vim = rainbow_delimiters.strategy["local"],
-        },
-        query = {
-          [""] = "rainbow-delimiters",
-          lua = "rainbow-blocks",
-        },
-        highlight = {
-          "RainbowDelimiterRed",
-          "RainbowDelimiterYellow",
-          "RainbowDelimiterBlue",
-          "RainbowDelimiterOrange",
-          "RainbowDelimiterGreen",
-          "RainbowDelimiterViolet",
-          "RainbowDelimiterCyan",
-        },
-        blacklist = { "c", "cpp" },
-      }
-    end,
   },
 
   -- indent guides
