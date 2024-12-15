c9a99cfbd3bc833fb42488ee8bdb6974e6fa7f3a
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-12-15 16:45:03 +0300

    refactor(nvim): avante lazyload

--- nvim/lua/plugins/completion/init.lua
+++ nvim/lua/plugins/completion/init.lua
@@ -270,7 +270,6 @@ return {
     "yetone/avante.nvim",
     enabled = true,
     event = "VeryLazy",
-    lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
       -- add any opts here
