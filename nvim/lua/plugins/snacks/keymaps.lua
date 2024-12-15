d81e8c971ba5d7889a86af93a8127f9d7b59cf01
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-12-15 16:54:57 +0300

    feat(nvim): snacks keybindings

--- nvim/lua/plugins/snacks/keymaps.lua
+++ nvim/lua/plugins/snacks/keymaps.lua
@@ -9,18 +9,51 @@ M = {
     desc = "Delete Buffer",
   },
   {
-    "<leader>gg",
-    function()
-      Snacks.lazygit()
-    end,
-    desc = "Lazygit",
-  },
-  {
     "<C-t>",
     function()
       Snacks.terminal()
     end,
     desc = "Toggle Terminal",
+  },
+
+  {
+    "]]",
+    function()
+      Snacks.words.jump(vim.v.count1)
+    end,
+    desc = "Next Reference",
+    mode = { "n", "t" },
+  },
+  {
+    "[[",
+    function()
+      Snacks.words.jump(-vim.v.count1)
+    end,
+    desc = "Prev Reference",
+    mode = { "n", "t" },
+  },
+
+  {
+    "<leader>z",
+    function()
+      Snacks.zen()
+    end,
+    desc = "Toggle Zen Mode",
+  },
+  {
+    "<leader>Z",
+    function()
+      Snacks.zen.zoom()
+    end,
+    desc = "Toggle Zoom",
+  },
+
+  {
+    "<leader>cR",
+    function()
+      Snacks.rename.rename_file()
+    end,
+    desc = "Rename File",
   },
 }
 return M
