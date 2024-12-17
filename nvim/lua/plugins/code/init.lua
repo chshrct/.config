4bbe4e07464d7db8b943e29ac6fa445f583fbcc6
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-12-17 11:44:10 +0300

    refactor(nvim): mini.pairs

--- nvim/lua/plugins/code/init.lua
+++ nvim/lua/plugins/code/init.lua
@@ -8,16 +8,20 @@ return {
 
   -- autopairs
   {
-    "windwp/nvim-autopairs",
+    "echasnovski/mini.pairs",
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-    config = function()
-      require("nvim-autopairs").setup({})
-
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      local cmp = require("cmp")
-
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end,
+    opts = {
+      modes = { insert = true, command = true, terminal = false },
+      -- skip autopair when next character is one of these
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      -- skip autopair when the cursor is inside these treesitter nodes
+      skip_ts = { "string" },
+      -- skip autopair when next character is closing pair
+      -- and there are more closing pairs than opening pairs
+      skip_unbalanced = true,
+      -- better deal with markdown code blocks
+      markdown = true,
+    },
   },
 
   -- rainbow brackets
