56ab854807e1c700187faf3c7db2c8d22c0d0685
Yauheni Kastrama <eugene.kostroma@gmail.com>
2024-12-28 23:16:24 +0300

    feat(nvim): add session persistence plugin

---
+++ nvim/lua/plugins/sessions.lua
@@ -0,0 +1,35 @@
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre",
+  opts = {},
+  keys = {
+    {
+      "<leader>pr",
+      function()
+        require("persistence").load()
+      end,
+      desc = "[p]ersistence [r]estore",
+    },
+    {
+      "<leader>sp",
+      function()
+        require("persistence").select()
+      end,
+      desc = "[s]earch [p]ersistence",
+    },
+    {
+      "<leader>pl",
+      function()
+        require("persistence").load({ last = true })
+      end,
+      desc = "[p]ersistence [l]oad",
+    },
+    {
+      "<leader>ps",
+      function()
+        require("persistence").stop()
+      end,
+      desc = "[p]ersistence [s]top",
+    },
+  },
+}
