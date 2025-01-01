161a9f18884f816f4699f36fc42bdd87f6bcb161
Yauheni Kastrama <eugene.kostroma@gmail.com>
2025-01-02 01:15:29 +0300

    refactor(nvim): setup vtsls

--- nvim/lua/plugins/lsp/servers.lua
+++ nvim/lua/plugins/lsp/servers.lua
@@ -36,6 +36,32 @@ M.servers = {
   eslint = {},
   emmet_language_server = {},
   vtsls = {
+    on_attach = function(client, bufnr)
+      vim.keymap.set("n", "<leader>lio", function()
+        vim.lsp.buf.code_action({
+          apply = true,
+          context = {
+            only = { "source.organizeImports" },
+          },
+        })
+      end, { buffer = bufnr, desc = "[l]sp [i]mports [o]rganize" })
+      vim.keymap.set("n", "<leader>lia", function()
+        vim.lsp.buf.code_action({
+          apply = true,
+          context = {
+            only = { "source.addMissingImports" },
+          },
+        })
+      end, { buffer = bufnr, desc = "[l]sp [i]mports [a]dd" })
+      vim.keymap.set("n", "<leader>lF", function()
+        vim.lsp.buf.code_action({
+          apply = true,
+          context = {
+            only = { "source.fixAll" },
+          },
+        })
+      end, { buffer = bufnr, desc = "[l]sp [F]ix all" })
+    end,
     -- explicitly add default filetypes, so that we can extend
     -- them in related extras
     settings = {
