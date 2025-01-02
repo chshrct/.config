9a0076943edb647e15a948b054bd3361b61d26bc
Yauheni Kastrama <eugene.kostroma@gmail.com>
2025-01-03 00:52:18 +0300

    feat(nvim): mini.files

--- nvim/lua/plugins/navigation/keymaps.lua
+++ nvim/lua/plugins/navigation/keymaps.lua
@@ -15,6 +15,23 @@ M.nvimtree = {
   },
 }
 
+M.minifiles = {
+  {
+    "<leader>m",
+    function()
+      require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+    end,
+    desc = "[m]ini files (Current file directory)",
+  },
+  {
+    "<leader>M",
+    function()
+      require("mini.files").open(vim.uv.cwd(), true)
+    end,
+    desc = "[M]ini files (Current working directory)",
+  },
+}
+
 M.fzf = function()
   return {
     -- files and strings
