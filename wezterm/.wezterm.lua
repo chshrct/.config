bf894afbb6fdbbbe1d1bbefe38c7e659824a0e79
Yauheni Kastrama <eugene.kostroma@gmail.com>
2025-01-03 18:14:31 +0300

    refactor(wezterm): fix paddings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,6 +5,14 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- padding
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
 -- term
 config.front_end = "OpenGL"
 config.max_fps = 144
