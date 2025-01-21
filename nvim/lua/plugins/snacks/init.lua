bdf05ea615a98cba6237eb410c950d750dd02be6
Yauheni Kastrama <eugene.kostroma@gmail.com>
2025-01-21 18:00:49 +0300

    refactor(nvim): remove dressing

--- nvim/lua/plugins/snacks/init.lua
+++ nvim/lua/plugins/snacks/init.lua
@@ -10,6 +10,7 @@ return {
     words = {},
     indent = { enabled = false },
     scroll = { enabled = false },
+    input = {},
     scratch = {
       name = "Notes",
       ft = "markdown",
--- nvim/lua/plugins/ui/init.lua
+++ nvim/lua/plugins/ui/init.lua
@@ -92,24 +92,6 @@ return {
     end,
   },
 
-  -- dressing
-  {
-    "stevearc/dressing.nvim",
-    lazy = true,
-    init = function()
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.select = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.select(...)
-      end
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.input = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.input(...)
-      end
-    end,
-  },
-
   -- lightbulb
   {
     "kosayoda/nvim-lightbulb",
