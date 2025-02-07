6f96df718d97f8356b094851018733dd70f5e230
Yauheni Kastrama <eugene.kostroma@gmail.com>
2025-02-07 12:21:35 +0300

    refactor(nvim): lsp keymaps

--- nvim/lua/plugins/lsp/keymaps.lua
+++ nvim/lua/plugins/lsp/keymaps.lua
@@ -10,23 +10,23 @@ M.lsp = function(event)
   vim.keymap.set("n", "<leader>li", ":Trouble lsp_implementations focus=true<CR>",
     { buffer = event.buf, desc = "[l]sp: goto [i]mplementation", silent = true })
   vim.keymap.set("n", "<leader>lt", ":Trouble lsp_type_definitions focus=true<CR>",
-    { buffer = event.buf, desc = "lsp: type [D]efinition", silent = true })
-  vim.keymap.set("n", "<leader>ds", Snacks.picker.lsp_symbols,
-    { buffer = event.buf, desc = "lsp: [d]ocument [s]ymbols", silent = true, })
-  vim.keymap.set("n", "<leader>ws", Snacks.picker.lsp_workspace_symbols,
-    { buffer = event.buf, desc = "lsp: [w]orkspace [s]ymbols", silent = true })
+    { buffer = event.buf, desc = "[l]sp: [t]ype definition", silent = true })
+  vim.keymap.set("n", "<leader>ls", Snacks.picker.lsp_symbols,
+    { buffer = event.buf, desc = "[l]sp: document [s]ymbols", silent = true, })
+  vim.keymap.set("n", "<leader>lS", Snacks.picker.lsp_workspace_symbols,
+    { buffer = event.buf, desc = "[l]sp: workspace [S]ymbols", silent = true })
 
   -- See `:help K` for why this keymap
   vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf, desc = "lsp: hover documentation" })
 
   -- Lesser used LSP functionality
   vim.keymap.set("n", "<leader>ld", vim.lsp.buf.declaration, { buffer = event.buf, desc = "[l]sp: goto [d]eclaration" })
-  vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder,
-    { buffer = event.buf, desc = "lsp: [w]orkspace [a]dd folder" })
-  vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder,
-    { buffer = event.buf, desc = "lsp: [w]orkspace [r]emove folder" })
-  vim.keymap.set("n", "<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
-    { buffer = event.buf, desc = "lsp: [w]orkspace [l]ist folders" })
+  vim.keymap.set("n", "<leader>lwa", vim.lsp.buf.add_workspace_folder,
+    { buffer = event.buf, desc = "[l]sp: [w]orkspace [a]dd folder" })
+  vim.keymap.set("n", "<leader>lwr", vim.lsp.buf.remove_workspace_folder,
+    { buffer = event.buf, desc = "[l]sp: [w]orkspace [r]emove folder" })
+  vim.keymap.set("n", "<leader>lwl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+    { buffer = event.buf, desc = "[l]sp: [w]orkspace [l]ist folders" })
 end
 
 M.format = function(conform)
